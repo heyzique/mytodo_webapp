9da7ffdb4dbf679565b7bbc82bd51cd072dfe43f
heyzique <144900297+heyzique@users.noreply.github.com>
2023-11-23 16:26:09 +0800

    create a checkbox that displays existing to-do items & add in new created items

--- web.py
+++ web.py
@@ -17,5 +17,14 @@ st.title("My To-Do App")
 st.subheader("The app that increases your productivity")
 st.write("Try it! Create your list now!")
 
+# List existing items & add new items that are stored
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
+
 st.text_input(label="-", placeholder="Add new to-do...",
               on_change=add_todo, key='new_todo')
