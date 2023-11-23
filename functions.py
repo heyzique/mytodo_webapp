fe55706f7bf2ec3497559efe7eacb2be25c4f292
heyzique <144900297+heyzique@users.noreply.github.com>
2023-11-23 16:17:52 +0800

    Create function to open and write text files

---
+++ functions.py
@@ -0,0 +1,23 @@
+# Constant variable are in all caps
+FILEPATH = "todo.txt"
+
+
+# Custom Function
+def get_todos(filepath=FILEPATH):
+    # doc-string is to help other developer looking at your code
+    """
+    Read a text file and return the list
+    to do items.
+    """
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+# non-default parameter has to come before default param
+def write_todos(todos_arg, filepath=FILEPATH):
+    """
+    Write the to-do items list in the text file.
+    """
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
--- web.py
+++ web.py
@@ -3,3 +3,5 @@ import streamlit as st
 st.title("My To-Do App")
 st.subheader("The app that increases your productivity")
 st.write("Try it! Create your list now!")
+
+st.text_input(label="-", placeholder="Add new to-do...", key='new_todo')
